a34ad1cc7d9bdf45c0a390b83cdf8e1fca98e335
thomas-rocke <tom@rocke.co.uk>
2023-10-18 19:14:01 +0100

    Expose clustering.f95 to f90wrap; cur_scores wrapper

---
+++ quippy/quippy/clustering.py
@@ -0,0 +1,25 @@
+from quippy.clustering_module import cur_scores
+import numpy as np
+
+
+def get_cur_scores(vecs):
+    '''
+    Perform a CUR decomposition on the NxM array vecs, returning an N-length array of CUR scores
+    '''
+
+    assert len(vecs.shape) in [1, 2]
+
+    if len(vecs.shape) == 1:
+        arr = vecs[:, np.newaxis].copy()
+    else:
+        arr = vecs.copy()
+    N = arr.shape[0]
+
+    scores = np.zeros(N, dtype=np.float64)
+
+    cur_scores(arr.T, scores)
+
+    # Normalise scores
+    scores /= np.sum(scores)
+
+    return scores
